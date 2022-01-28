a9a90442b1be59f4582efc5ad403f4bf6d435ef4
Mark Etale <66547466+Mark-code789@users.noreply.github.com>
2022-01-28 22:16:51 +0300

    update index.js

--- index.js
+++ index.js
@@ -446,6 +446,7 @@ class Stream {
             await this.worker.load();
             await this.worker.loadLanguage('eng');
             await this.worker.initialize('eng');*/
+            alert("ALERT\n\nPlease note this app is under maintenance will notify you the it is over.");
             LoadingDone();
         } catch (error) {
             console.log(error);
