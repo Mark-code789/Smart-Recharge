05795fc5b7f1f22f97105d127b26e4a913b4b12a
Mark Etale <66547466+Mark-code789@users.noreply.github.com>
2022-01-29 17:41:45 +0300

    update index.js

--- index.js
+++ index.js
@@ -129,9 +129,9 @@ class Options {
 		$(".about_window").classList.toggle("dark_theme");
 		$(".recharged").classList.toggle("dark_theme");
 		$(".load").classList.toggle("dark_theme");
+		let darkTheme = $(".toggle").classList.contains("toggle_switch");
 		if(window.storage) 
-			window.storage.setItem("theme", $(".toggle").classList.contains("toggle_switch") + "");
-		alert(e.target);
+			window.storage.setItem("theme",  darkTheme);
 	} 
     static share = () => {
         if(navigator.canShare) {
