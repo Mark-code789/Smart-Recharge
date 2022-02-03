8faae805d2bb290c0cb70f9266f530b29cc36b1c
Mark Etale <66547466+Mark-code789@users.noreply.github.com>
2022-02-03 17:20:05 +0300

    update index.js

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ async function load (i = 0) {
     if(response.status == 200) {
         let arrBuff = await response.arrayBuffer();
         if(arrBuff.byteLength > 0) {
-        	alert(src);
             src = await URL.createObjectURL(new Blob([arrBuff], {type: "image/png"}));
             document.documentElement.style.setProperty(imageProps[i], `url(${src})`);
             if(i == 3) 
